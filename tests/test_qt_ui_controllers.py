ee59ee474c3d4bd9192e228ba13fc40dc3e32f95
Ruben <unborn998@gmail.com>
2026-08-16 03:59:10 +0200

    whats new text

--- tests/test_qt_ui_controllers.py
+++ tests/test_qt_ui_controllers.py
@@ -789,7 +789,13 @@ class QtUiControllerTests(unittest.TestCase):
         self.assertEqual("What's new", runtime.dialogs.state_snapshot()["title"])
         message = runtime.dialogs.state_snapshot()["message"]
         self.assertEqual(
-            "• Beta; added stellar detection (experimental)",
+            "\n".join(
+                (
+                    "• Beta; added stellar detection (experimental)",
+                    "• RB 31-35 for all cycles now live on the tracker",
+                    "• We have added 24/7 Limited Deal alerts to the Discord as well as fast update alerts",
+                )
+            ),
             message,
         )
 
